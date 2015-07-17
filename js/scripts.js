ee3a4225a39303a9d6f4b9adc38f0f314f0151b4
Marcus Veres <marcus.veres@juicemobile.com>
2015-07-17 13:56:20 -0400

    pulls recipes.json data into the javascript

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,36 @@
 
     console.log("loaded scripts");
 
+    // vars
+    var recipes = 'unset';
+
+
+    // get our recipe data
+    var get_recipes = function()
+    {
+        $.ajax({
+            method: 'GET',
+            url: '/data/recipes.json'
+        })
+        .success( function( data )
+        {
+            console.log( "the data is:" , data );
+            recipes = data;
+        });
+
+    };
+
+
+    // initialize everything before page load
+    function init()
+    {
+        // populate the recipes variable
+        get_recipes();
+    }
+
+
     // page is ready
-    $(document).ready( function({
+    $(document).ready( function(){
 
         console.log("all systems go");
 
